541a1f6a542f7f2bc9bceaef39ec5a4fd2663530
Jacob Waclawski <54681308+jmw3638@users.noreply.github.com>
2023-04-02 22:21:53 -0400

    Simple items and blocks for getting familiar

---
+++ src/main/java/com/jakewac/theboysmod/init/TheBoysModCreativeTab.java
@@ -0,0 +1,21 @@
+package com.jakewac.theboysmod.init;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+public class TheBoysModCreativeTab extends CreativeModeTab {
+
+    public static final TheBoysModCreativeTab instance = new TheBoysModCreativeTab(CreativeModeTab.TABS.length,
+            "The Boys Mod");
+
+    private TheBoysModCreativeTab(int index, String label) {
+        super(index, label);
+    }
+
+    @Override
+    public ItemStack makeIcon() {
+        return new ItemStack(Items.SLIME_BALL);
+    }
+
+}
---
+++ src/main/java/com/jakewac/theboysmod/items/TeleportItem.java
@@ -0,0 +1,68 @@
+package com.jakewac.theboysmod.items;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+
+public class TeleportItem extends Item {
+
+    public TeleportItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+        tooltip.add(Component.literal("Teleports you to where you are looking"));
+
+        super.appendHoverText(stack, worldIn, tooltip, flagIn);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        BlockHitResult ray = rayTrace(world, player, ClipContext.Fluid.NONE);
+        BlockPos lookPos = ray.getBlockPos().relative(ray.getDirection());
+
+        player.setPos(lookPos.getX(), lookPos.getY(), lookPos.getZ());
+
+        player.fallDistance = 0F;
+
+        player.getCooldowns().addCooldown(this, 60);
+
+        world.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.ENDERMAN_TELEPORT,
+                SoundSource.PLAYERS, 1.0F, 1.0F);
+
+        return super.use(world, player, hand);
+    }
+
+    protected static BlockHitResult rayTrace(Level world, Player player, ClipContext.Fluid fluidMode) {
+        double range = 15;
+
+        float f = player.getXRot();
+        float f1 = player.getYRot();
+        Vec3 vector3d = player.getEyePosition(1.0F);
+        float f2 = Mth.cos(-f1 * ((float) Math.PI / 180F) - (float) Math.PI);
+        float f3 = Mth.sin(-f1 * ((float) Math.PI / 180F) - (float) Math.PI);
+        float f4 = -Mth.cos(-f * ((float) Math.PI / 180F));
+        float f5 = Mth.sin(-f * ((float) Math.PI / 180F));
+        float f6 = f3 * f4;
+        float f7 = f2 * f4;
+        Vec3 vector3d1 = vector3d.add((double) f6 * range, (double) f5 * range, (double) f7 * range);
+        return world.clip(new ClipContext(vector3d, vector3d1, ClipContext.Block.OUTLINE, fluidMode, player));
+    }
+}
